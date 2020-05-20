06fc635fd9a73b7c222e87d4cc6138938a36e23b
Nguyen Nguyen <nguyentwotimes@gmail.com>
2020-05-20 17:36:09 -0400

    Navigation bar set-up

--- web-app/src/App.vue
+++ web-app/src/App.vue
@@ -1,32 +1,84 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <b-navbar type="black" variant="white">
+      <b-navbar-nav>
+            <b-nav-item id="home" href="/">Nguyen Nguyen</b-nav-item>
+            <b-nav-item href="/projects">Projects</b-nav-item>
+            <b-nav-item href="/contact">Contact</b-nav-item>
+      </b-navbar-nav>
+       </b-navbar>
     </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
 }
 
 #nav {
-  padding: 30px;
-
+  padding: 47px;
   a {
     font-weight: bold;
     color: #2c3e50;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #31a06e;
     }
   }
 }
+
+// (home bar hover effect)
+#home {
+  
+  transition: all .35s ease;
+}
+
+#home a {
+  position: relative;
+}
+
+#home a::before,
+#home a::after {
+  content: '';
+  height: 15px;
+  width: 15px;
+  position: absolute;
+  transition: all .35s ease;
+  opacity: 0;
+}
+
+#home a::before {
+  content: '';
+  right : 0px;
+  top: 0;
+  border-top: 2px solid #3E8914;
+  border-right: 2px solid #2E640F;
+  transform: translate(-100%, 100%);
+}
+
+#home a:after {
+  content: '';
+  left: 0;
+  bottom: 0;
+  border-bottom: 2px solid #2E640F;
+  border-left: 2px solid #3E8914;
+  transform: translate(100%, -100%)
+}
+
+#home a:hover:before,
+#home a:hover:after{
+  transform: translate(0,0);
+  opacity: 1;
+}
+// (end of hover effect)
 </style>
--- web-app/src/router/index.js
+++ web-app/src/router/index.js
@@ -11,13 +11,22 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/projects",
+    name: "projects",
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (projects.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "projects" */ "../views/projects.vue")
+  },
+  {
+    path: "/contact",
+    name: "contact",
+    // route level code-splitting
+    // this generates a separate chunk (projects.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "projects" */ "../views/contact.vue")
   }
 ];
 
